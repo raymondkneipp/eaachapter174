6844525e9c57eb09a8b46a1618f7dfed34455683
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-10-06 16:58:18 -0400

    change breakpoint to stack on larger screen

--- src/components/_layout/Wide/Wide.tsx
+++ src/components/_layout/Wide/Wide.tsx
@@ -7,7 +7,7 @@ const Wide: React.VFC<Props> = ({ master, slave }) => {
 	return (
 		<div className="space-y-12">
 			<div className="space-y-6">{master}</div>
-			<div className="grid grid-cols-1 md:grid-cols-3 gap-6">{slave}</div>
+			<div className="grid grid-cols-1 lg:grid-cols-3 gap-6">{slave}</div>
 		</div>
 	);
 };
