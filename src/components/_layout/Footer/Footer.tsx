c9a22aa985911255054e9fdc267b06138378eb54
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-10-06 16:58:41 -0400

    add facebook and remove copyright

--- src/components/_layout/Footer/Footer.tsx
+++ src/components/_layout/Footer/Footer.tsx
@@ -1,13 +1,21 @@
-import { Container, Typography, PageLink } from "@components";
+import { Container, PageLink } from '@components';
+
+import { FaFacebook } from 'react-icons/fa';
 
 const Footer: React.VFC = () => {
 	return (
 		<footer className="bg-gray-100 py-6">
 			<Container>
 				<div className="flex flex-col items-center space-y-3 md:space-y-0 md:flex-row justify-between text-center">
-					<Typography variant="p">
-						&copy; {new Date().getFullYear()} EAA Chapter 174
-					</Typography>
+					<a
+						href="https://www.facebook.com/eaachapter174/"
+						className="text-gray-600 hover:text-black transition flex items-center gap-3"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<FaFacebook size={18} />
+						Facebook
+					</a>
 
 					<PageLink
 						to="https://raymondkneipp.com"
