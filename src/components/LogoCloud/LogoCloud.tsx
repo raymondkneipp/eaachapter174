856e2b87125d9b7eb09449f9042d584b0e90838e
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-10-06 22:26:09 -0400

    remove logo cloud text

--- src/components/LogoCloud/LogoCloud.tsx
+++ src/components/LogoCloud/LogoCloud.tsx
@@ -1,17 +1,14 @@
-import { Tile, Container } from "@components";
-import LogoCloudItem from "./LogoCloudItem/LogoCloudItem";
+import { Container, Tile } from '@components';
+import LogoCloudItem from './LogoCloudItem/LogoCloudItem';
 
 const Brands: React.VFC = () => {
 	return (
 		<Container>
 			<Tile>
-				<LogoCloudItem src="EAA_Chapters.png" name="EAA Chapters" />
-				<LogoCloudItem
-					src="EAA_logo.png"
-					name="Experimental Aircraft Association"
-				/>
-				<LogoCloudItem src="YE25th.png" name="Young Eagles 25th Anniversary" />
-				<LogoCloudItem src="YE.png" name="Young Eagles" />
+				<LogoCloudItem src="EAA_Chapters.png" />
+				<LogoCloudItem src="EAA_logo.png" />
+				<LogoCloudItem src="YE25th.png" />
+				<LogoCloudItem src="YE.png" />
 			</Tile>
 		</Container>
 	);
--- src/components/LogoCloud/LogoCloudItem/LogoCloudItem.tsx
+++ src/components/LogoCloud/LogoCloudItem/LogoCloudItem.tsx
@@ -1,28 +1,21 @@
-import Typography from "components/Typography/Typography";
-import Image from "next/image";
+import Image from 'next/image';
 
 interface Props {
 	src: string;
-	name: string;
 }
 
-const LogoCloudItem: React.VFC<Props> = ({ src, name }) => {
+const LogoCloudItem: React.VFC<Props> = ({ src }) => {
 	return (
-		<div className="flex items-center flex-col text-center sm:flex-row sm:text-left sm:space-x-6 md:flex-col md:space-x-0 md:text-center xl:flex-row xl:space-x-6 xl:text-left">
-			<div className="opacity-30">
-				<Image
-					src={`/img/badges/${src}`}
-					layout="fixed"
-					width="100"
-					height="100"
-					objectFit="contain"
-					alt={name}
-					priority={true}
-				/>
-			</div>
-			<Typography element="h4" variant="h6">
-				{name}
-			</Typography>
+		<div className="flex items-center justify-center opacity-30">
+			<Image
+				src={`/img/badges/${src}`}
+				layout="fixed"
+				width="100"
+				height="100"
+				objectFit="contain"
+				alt={'badge'}
+				priority={true}
+			/>
 		</div>
 	);
 };
